26b1a0365d5b758ff7732302611138cb02848dd7
Jian James P. Astrero <jiana@goetu.com>
2020-05-18 23:45:08 +0800

    auto move to main activity

--- app/src/main/java/com/jianastrero/yelpr/activities/SplashActivity.kt
+++ app/src/main/java/com/jianastrero/yelpr/activities/SplashActivity.kt
@@ -1,13 +1,28 @@
 package com.jianastrero.yelpr.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jianastrero.yelpr.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Default).launch {
+            delay(1500)
+
+            withContext(Dispatchers.Main) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+        }
     }
 }
